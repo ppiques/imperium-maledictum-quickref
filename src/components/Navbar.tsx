872c81539fc1c6b6fb87e9e66ab686f779e284ca
ppiques <ppiques@student.42.fr>
2025-04-08 12:04:33 +0200

    feat: add navbar menu for mobile navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,73 +1,111 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import "./Navbar.css";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
-        <Link className="navbar-brand" to="/">
+        <Link className="navbar-brand" to="/" onClick={handleLinkClick}>
           <span className="navbar-logo">⚙</span> Imperium Maledictum
         </Link>
         <button
           className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
+          onClick={toggleNavbar}
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          ☰
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div
+          className={`collapse navbar-collapse ${isOpen ? "active" : ""}`}
+          id="navbarNav"
+        >
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/">
+              <Link className="nav-link" to="/" onClick={handleLinkClick}>
                 Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/augmetics">
+              <Link
+                className="nav-link"
+                to="/augmetics"
+                onClick={handleLinkClick}
+              >
                 Augmetics
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/talents">
+              <Link
+                className="nav-link"
+                to="/talents"
+                onClick={handleLinkClick}
+              >
                 Character
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/Combat">
+              <Link className="nav-link" to="/combat" onClick={handleLinkClick}>
                 Combat
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/equipment">
+              <Link
+                className="nav-link"
+                to="/equipment"
+                onClick={handleLinkClick}
+              >
                 Equipment
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/protection">
+              <Link
+                className="nav-link"
+                to="/protection"
+                onClick={handleLinkClick}
+              >
                 Protection
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/psy">
+              <Link className="nav-link" to="/psy" onClick={handleLinkClick}>
                 Psykers
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/services">
+              <Link
+                className="nav-link"
+                to="/services"
+                onClick={handleLinkClick}
+              >
                 Services
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/weapons">
+              <Link
+                className="nav-link"
+                to="/weapons"
+                onClick={handleLinkClick}
+              >
                 Weapons
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/criticalwounds">
+              <Link
+                className="nav-link"
+                to="/criticalwounds"
+                onClick={handleLinkClick}
+              >
                 Wounds & Injuries
               </Link>
             </li>
